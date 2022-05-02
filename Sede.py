2b434862006cd3d8fa3102f71ce202d46c1eb4c2
josecarlos20 <78504539+josecarlos20@users.noreply.github.com>
2022-05-02 17:10:11 -0500

    Add files via upload

---
+++ Sede.py
@@ -0,0 +1,10 @@
+
+class Sede:
+    def __init__(self, direccion, proveedor, administrador):
+        self.direccion = direccion
+        self.proveedor = proveedor
+        self.administrador = administrador
+
+    def sede(self):
+        pass
+    
