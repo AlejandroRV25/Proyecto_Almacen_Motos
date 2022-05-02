a56a0174dc1f6d4a10df6c8e67a41a2e446295c0
AlejandroRV25 <78456255+AlejandroRV25@users.noreply.github.com>
2022-05-02 17:21:12 -0500

    Add files via upload

---
+++ Servicio.py
@@ -0,0 +1,34 @@
+
+class Servicio:
+    def __init__(self, precio, fecha, tipo, problema):
+        self.precio = precio
+        self.fecha = fecha
+        self.tipo = tipo
+        self.problema = problema
+
+    def servicio(self):
+        pass
+
+    def get_precio(self):
+        return self.precio
+
+    def get_fecha(self):
+        return self.fecha
+
+    def get_tipo(self):
+        return self.tipo
+
+    def get_problema(self):
+        return self.problema
+
+    def set_precio(self, precio):
+        self.precio = precio
+
+    def set_fecha(self, fecha):
+        self.fecha = fecha
+
+    def set_tipo(self, tipo):
+        self.tipo = tipo
+
+    def set_problema(self, problema):
+        self.problema = problema
