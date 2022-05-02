1b271c7e1362925ceb7d32df1571e7073b71f549
josecarlos20 <78504539+josecarlos20@users.noreply.github.com>
2022-05-01 20:57:54 -0500

    Add files via upload

---
+++ Producto.py
@@ -0,0 +1,24 @@
+
+class Producto:
+    def __init__(self, id, nomProduc, precio):
+        self.id = id
+        self.nomProduc = nomProduc
+        self.precio = precio
+
+    def producto(self):
+        pass
+
+    def get_id(self):
+        pass
+
+    def get_nombre(self):
+        pass
+
+    def set_id(self, id):
+        pass
+
+    def set_nombre(self, nombre):
+        pass
+
+    def set_precio(self, precio):
+        pass
