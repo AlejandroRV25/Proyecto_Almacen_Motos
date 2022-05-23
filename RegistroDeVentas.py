09c68589b41c9b7f469bf61957c77606865beb36
josecarlos20 <78504539+josecarlos20@users.noreply.github.com>
2022-05-22 21:47:15 -0500

    Update RegistroDeVentas.py

--- RegistroDeVentas.py
+++ RegistroDeVentas.py
@@ -1,10 +1,23 @@
-
+import os
+import csv
 class RegistroDeVentas:
-    def __init__(self, servicio, factura, registro):
+    def __init__(self, servicio:servicio, factura:factura, RegistroVentas:RegistroVentas):
         self.servicio = servicio
         self.factura = factura
         self.registro = registro
 
     def registro_ventas(self):
         pass
+    
+    def RegistroDeVentas(registro):
+        cantidad = int(input('registro de ventas'))
+        
+        with open(registro, 'a', newline='') as archivo_csv:
+            writer = csv.writer(archivo_csv, delimiter=',')
+            for i in range(cantidad):
+                os.system('cls')
+                servicio = servicio ('servicio: ')
+                factura = factura ('factura: ')
+                RegistroVentas = RegistroVentas ('Registro de Ventas: ')
+                
     
