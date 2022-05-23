87109b4f993b56e2ab97a9e3c4ed09bc15b6befc
josecarlos20 <78504539+josecarlos20@users.noreply.github.com>
2022-05-22 22:18:13 -0500

    Update Factura.py

--- Factura.py
+++ Factura.py
@@ -1,23 +1,16 @@
 import os
 import csv
 class Factura:
-    def __init__(self, producto:producto, cliente:cliente, fecha:string, numFactura:int):
+    def __init__(self, producto:producto, cliente:cliente):
         self.producto = producto
         self.cliente = cliente
-        self.fecha = fecha
-        self.numFactura = numFactura
+
 
     def factura(self):
         pass
 
-    def get_fecha(self):
-        return self.fecha
-
     def get_numFactura(self):
         return self.numFactura
-
-    def set_fecha(self, fecha):
-        self.fecha = fecha
 
     def set_numFactura(self, numFactura):
         self.numFactura = numFactura
