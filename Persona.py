2eeaa54871ce0174afc095beb0cd9beb31c54080
josecarlos20 <78504539+josecarlos20@users.noreply.github.com>
2022-05-22 22:26:41 -0500

    Update Persona.py

--- Persona.py
+++ Persona.py
@@ -1,32 +1,17 @@
 
 class Personal:
-    def __init__(self, nombre, datoContacto, rh, documento, ventas):
-        self.nombre = nombre
-        self.datoContacto = datoContacto
+    def __init__(self, rh:string, ventas:int):
         self.rh = rh
-        self.documento = documento
         self.ventas = ventas
-
-    def get_nombre(self):
-        return self.nombre
 
     def get_rh(self):
         return self.rh
 
-    def get_documento(self):
-        return self.documento
-
     def get_ventas(self):
         return self.ventas
 
-    def set_nombre(self, nombre):
-        self.nombre = nombre
-
     def set_rh(self, rh):
         self.rh = rh
-
-    def set_documento(self, documento):
-        self.documento = documento
 
     def set_ventas(self, ventas):
         self.ventas = ventas
