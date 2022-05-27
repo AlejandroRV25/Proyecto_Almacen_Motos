954f4025fa9accff25d32e1853c42ff2663a8b18
AlejandroRV25 <78456255+AlejandroRV25@users.noreply.github.com>
2022-05-27 00:10:38 -0500

    Add files via upload

---
+++ Usuario.py
@@ -0,0 +1,19 @@
+
+class Usuario:
+    def __init__(self, nombre, documento, datoContacto):
+        self.nombre = nombre
+        self.documento = documento
+        self.datosContacto = datoContacto
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_documento(self):
+        return self.documento
+
+    def set_nombre(self, nombre):
+        self.nombProv = nombre
+
+    def set_direccion(self, documento):
+        self.documento = documento
+        
